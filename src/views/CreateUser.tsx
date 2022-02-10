f3c0a6699f75477c4fc630e71a85311b3842b6a1
Pedro Andrade Sartorato <phandradesar@hotmail.com>
2022-02-10 16:13:14 -0300

    estilização visual

--- src/views/CreateUser.tsx
+++ src/views/CreateUser.tsx
@@ -1,14 +1,97 @@
-import { TextField } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import React from 'react';
 
 export default function CreateUser() {
 
     return (
-        <div style={{ backgroundColor: 'red', height: '500px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <div style={{ backgroundColor: 'green', height: '100%', width: '45%' }}>
-                <TextField label={'Nome do usuario'} variant='outlined' style={{backgroundColor: 'white'}}/>
-            </div>
+        <div style={
+            {
+                backgroundColor: 'red',
+                height: '500px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }
+        }>
+            <div style={
+                {
+                    backgroundColor: 'green',
+                    height: '90%',
+                    width: '45%',
+                    display: 'flex',
+                    justifyContent: 'center'   /*display: 'flex'= ficar flexivel | justifyContent: 'center' =  o conteudo ficar centralizado*/
+                }
+            }>
+                <div style={{ width: '100%' }}>
 
+                    <div style={{
+                        marginBottom: '15px',
+                        width: '100%',
+                        display: 'flex',
+                        justifyContent: 'center'
+
+                    }}> {/*usado para fazer a margem de baixo do campo*/}
+
+                    </div>
+
+                    <div style={{
+                        marginBottom: '15px',
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}>
+                        <TextField
+                            label={'Nome do usuario'}
+                            variant='outlined'
+                            style={{
+                                backgroundColor: 'white',
+                                width: '50%',
+                            }} />
+                    </div>
+                    <div style={{
+                        marginBottom: '15px',
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}>
+                        <TextField
+                            label={'Email'}
+                            variant='outlined'
+                            style={{ 
+                                backgroundColor: 'white',
+                            width: '50%'
+                            }} />
+                    </div>
+                    <div style={{
+                        marginBottom: '15px',
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}>
+                        <TextField
+                        security=''
+                            label={'Senha'}
+                            variant='outlined'
+                            style={{ 
+                                backgroundColor: 'white',
+                                width: '50%',
+                                 }} />
+                    </div>
+                    <div style={{
+                        backgroundColor: 'black',
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}>
+                        <Button
+                            variant={'contained'}
+                            style={{
+                                height: '50px',
+                                width: '100px',
+                                backgroundColor: 'blue'
+                            }}>
+
+                            {'Criar'}
+                        </Button>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
